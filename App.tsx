bbe2e16588cd5e9d9764e15afb31586472439c1c
DenisMies <bhu243@myy.haaga-helia.fi>
2026-09-16 19:25:52 +0300

    Harjoitus6 palautus

--- App.tsx
+++ App.tsx
@@ -1,12 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Meal from './types';
+import { useState } from 'react';
+import { TextInput, FlatList, Image } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import { Button } from 'react-native';
 
 export default function App() {
+
+  const [meals, setMeals] = useState<Meal[]>([]);
+  const [ingredient, setIngredient] = useState("");
+
+  const fetchMeals = () => {
+    console.log("Haku alkaa");
+    fetch(`${process.env.EXPO_PUBLIC_API_URL}?i=${ingredient}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Something went wrong in fetch");
+        }
+
+        return response.json();
+      })
+      .then(data => setMeals(data.meals))
+      .catch(err => console.error(err))
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder='Ingredient'
+          value={ingredient}
+          onChangeText={setIngredient}
+        />
+        <Button
+          title='Find'
+          onPress={fetchMeals}
+        />
+        <FlatList
+          style={styles.list}
+          data={meals}
+          keyExtractor={(item) => item.idMeal}
+          renderItem={({ item }) =>
+            <View>
+              <Text>{item.strMeal}</Text>
+              <Image
+                source={{
+                  uri: item.strMealThumb
+                }}
+                style={styles.image}
+              />
+            </View>
+          }
+        />
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -17,4 +68,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 100,
+    height: 100,
+  },
+  input: {
+    marginTop: 20,
+    borderWidth: 0.5,
+    borderColor: "black",
+    borderRadius: 5,
+    padding: 10,
+  },
+  list: {
+    marginTop: 10,
+  }
 });
---
+++ types.ts
@@ -0,0 +1,7 @@
+type Meal = {
+    strMeal: string,
+    strMealThumb: string,
+    idMeal: string
+}
+
+export default Meal;
